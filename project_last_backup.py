e235944222da9b89dcc83e2185911f8139a3d0e4
VicVEVO <88597698+VicVEVO@users.noreply.github.com>
2021-08-07 23:24:00 +0200

    Update project_last_backup.py

--- project_last_backup.py
+++ project_last_backup.py
@@ -1,9 +1,9 @@
 
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jun 18 22:52:30 2021
-
-@author: victo
+Un programme du TIPE étudiant les transferts thermiques à 1D (sur une barre) avec la méthode de Crank-Nicolson.
+La matrice U représente la température, sa k-ième colonne correspond à la température sur la barre à l'instant k.
+                                        sa j-ième ligne correspond à la température au point j de la barre pour tout instant dans l'intervalle défini au début.
 """
 
 import numpy as np
@@ -11,30 +11,26 @@ from numpy.linalg import inv
 
 import seaborn as sns; sns.set_theme()
 
-precision = 70
+precision = 70 #paramètre temporaire bourrin qu'on utilise pour l'intervalle de mesure
 
 nlignes , ncolonnes = precision, precision
 
-xmax , tmax = 10 , 10
-
+xmax , tmax = 10 , 10 #Valeur de mesure spatiale (resp. temporelle) max
 alpha = 10**-5 # k/rho*c_p #coeff de diffusivité
 
+h_x , h_t = xmax/nlignes , tmax/ncolonnes #"correspondent" à dx , dt
+r = alpha*h_t/(h_x**2) #constante utilisée dans le calcul des matrices A et B pour la récurrence
 
-h_x , h_t = xmax/nlignes , tmax/ncolonnes
-r = alpha*h_t/(h_x**2)
 
-
-def f(x): #la fonction définissant la température U à t = 0
-    # return(np.exp(-0.1*x)*100)
-    # return(np.sin(x)*100 + 10)
-    # return(x**2-3*x+3)
+def f(x): #la fonction définissant la température U[0] sur la barre à t = 0
     return(abs(100*np.sin(x/2) + 20))
-    # return(x)
 
-def matrice_AB(r, nlignes, ncolonnes): #calcul des deux matrices A et B
+def matrice_AB(r, nlignes, ncolonnes): #calcul des deux matrices constantes A et B pour le calcul de récurrence du modèle
+    
     nlignes,ncolonnes = nlignes -2, ncolonnes-2
     A = np.zeros((nlignes,ncolonnes))
     B = np.zeros((nlignes,ncolonnes))
+    
     for i in range(nlignes):
         for j in range(ncolonnes):
             if i == j:
@@ -43,61 +39,37 @@ def matrice_AB(r, nlignes, ncolonnes): #calcul des deux matrices A et B
             elif (j == i + 1) or (i == j+1):
                 A[i][j] = -r
                 B[i][j] = r
-    return inv(A), B
+    return inv(A), B 
 
-
-
-def init(f,nlignes,ncolonnes): #initialise la matrice U
+def init(f,nlignes,ncolonnes): #initialisation de la matrice U
     M = np.zeros((nlignes,ncolonnes))
-    for i in range(nlignes):       
+    for i in range(nlignes): #température sur la barre à t = 0
         M[i][0] = f(h_x*i)
-    for i in range(ncolonnes):
+    for i in range(ncolonnes): #température constante aux bords de la barre
         M[0][i] = T0
         M[nlignes-1] = f((nlignes-1)*h_x)
+    return M
+
+def matrice_U(f, nlignes, ncolonnes, r, invA, B): #calcule la matrice U (lignes (resp. colonnes) correspondent aux x (resp. t)) par récurrence
     
-    return(M)
-
-def matrice_U(f, nlignes, ncolonnes, r, invA, B): #calcule la matrice U (lignes (resp. colonens) correspondent aux x (resp. t))
-    U = init(f,nlignes+2,ncolonnes+2)
-    #U = np.around(U,2)
-    # print('------------- Matrice initiale: -------------')
-    # print(U, '\n') #ctrl+1
-    # print(U)
-    invAxB = np.dot(invA,B)    #print(U[:,0])
+    U = init(f,nlignes+2,ncolonnes+2) #initialise U
+    invAxB = np.dot(invA,B) #calcule une fois A^-1*B pour éviter des calculs redondants
+    
     for t in range(1,ncolonnes+2):
+        
         b = np.zeros(nlignes)
-        #print('B_{} initial = {}'.format(t,b_t))
-        b[0] = r*U[0,t-1]
+        b[0] = r*U[0,t-1]   #calcul de la matrice colonne b, quasi nulle mais nécessaire dans le calcul par récurrence 
         b[nlignes-1] = r*U[nlignes+1,t-1]
-        print(b)
-# =============================================================================
-#         #print(r*U[nlignes+1,t]) 
-#         #print('B_{} final = {}'.format(t,b_t))
-#         
-#         #print(invAxB)
-#         #print((U[1:nlignes+1,t-1]))
-#         print(np.dot(invAxB,U[1:nlignes+1,t-1]))
-#         print(np.dot(invA,b_t))
-#         #print(np.dot(U[1:nlignes-1,t+1],invAxB))
-#         
-#         print(U[1:nlignes+1,t]) #= invAxB.dot(U[1:nlignes+1,t-1]) + invA.dot(b_t)
-#         print('')
-#         
-# =============================================================================
 
-        U[1:nlignes+1,t] = np.dot(invAxB,U[1:nlignes+1,t-1]) + np.dot(invA,b)
+        U[1:nlignes+1,t] = np.dot(invAxB,U[1:nlignes+1,t-1]) + np.dot(invA,b) #application de la formule de récurrence
+        
+    return U
 
-    return(U)
-
-T0 = f(0)
+"""   ### DÉBUT DU PROGRAMME ###   """
 invA,B = matrice_AB(r,nlignes+2,ncolonnes+2)
-
-
-
 U = matrice_U(f,nlignes, ncolonnes, r, invA, B)
 
-
-if precision <= 10: #On affiche correctement les label car sinon il y a trop: illisible & trop de temps d'affichage
+if precision <= 10: #On affiche pour l'instant les label que s'ils sont peu pour éviter l'illisibilité et le temps d'affichage trop coûteux
     ax = sns.heatmap(U,
                      xticklabels = np.around(np.arange(0, tmax + 2*h_t, h_t),1),
                      yticklabels = np.around(np.arange(0, xmax + 2*h_x, h_x),1),
@@ -111,9 +83,3 @@ else :
 ax.set(title="TEMPERATURE 1D",
       xlabel="Durée (s)",
       ylabel="Distance (m)")
-
-"""
-print("r = {}".format(r))
-print(np.round(inv(invA),1))
-print(np.round(B,1))
-print(np.around(U,2))"""
